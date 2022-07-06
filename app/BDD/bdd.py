8b4e6acc402e0ec903ee609e5b225dd13b71df89
V1cent <vincent.faure0@gmail.com>
2022-07-06 11:17:56 +0200

    clear code to enter data in database or read it

--- app/BDD/bdd.py
+++ app/BDD/bdd.py
@@ -1,17 +1,11 @@
 # sudo docker run --name db -e POSTGRES_PASSWORD=password -e POSTGRES_USER=admin -p 5432:5432 -d -v /home/vincent.faure@Digital-Grenoble.local/Documents/chat/BDD/:/var/lib/postgresql/data postgres
 
-from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, String, MetaData
+from sqlalchemy import Integer, create_engine  
+from sqlalchemy import Column, String
+import sqlalchemy  
+from sqlalchemy.ext.declarative import declarative_base  
+from sqlalchemy.orm import sessionmaker
 
-
-class User:
-    def __init__(self, nom, mail, tel="", message="", texte="", resume=""):
-        self.nom = nom
-        self.mail = mail
-        self.message = message
-        self.tel = tel
-        self.texte = texte
-        self.resume = resume
 
 
 # Postgres username, password, and database name
@@ -31,24 +25,60 @@ postgres_str = ('postgresql+psycopg2://{username}:{password}@{ipaddress}:{port}/
 print(postgres_str)
 db = create_engine(postgres_str)
 
-meta = MetaData(db)
-database = Table('user', meta,  
-                       Column('nom', String),
-                       Column('mail', String),
-                       Column('tel', String),
-                       Column('message', String),
-                       Column('texte', String),
-                       Column('resume', String))
+base = declarative_base()
 
-with db.connect() as conn:
+# Définition de la classe de l'objet enregistrer dans la base de donnée
+class Message(base):
+    __tablename__ = 'Message'
 
-    # database.create() # METTRE UN IF TABLE EXIST ....
-    insert_statement = database.insert().values(nom="Doctor Strange2", mail="doctor.strange2@doctor.com")
-    conn.execute(insert_statement)
+    id = Column(Integer, primary_key = True)
+    nom = Column(String)
+    mail = Column(String)
+    tel = Column(String)
+    message = Column(String)
+    texte = Column(String)
+    resume = Column(String)
 
+    def __str__(self):
+        message = f"id = {self.id},\nnom = {self.nom},\nmail = {self.mail},\ntel = {self.tel},\nmessage = {self.message},\ntexte = {self.texte},\nresume = {self.resume}\n\n"
+        return message
+
+# Connection à la base de donnée
+Session = sessionmaker(db)  
+session = Session()
+
+
+# Vérification de l'existence de la base de donnée
+if not sqlalchemy.inspect(db).has_table('Message'):
+    base.metadata.create_all(db)
+
+
+def add_entry_bdd(nom, mail, tel="", message="", texte="", resume=""):
+    '''
+    add_entry_bdd permet d'ajouter une entrée à la base de donnée\n
+    Input:  
+        * nom, mail, tel, message, texte, resume corespond au donnée du formulaire
+    '''
+    # Create 
+    entry = Message(nom=nom, mail=mail, tel=tel, message=message, texte=texte, resume=resume)  
+    session.add(entry)  
+    session.commit()
+
+def read_all_bdd():
+    '''
+    read_all_bdd affiche la totalité de la base de donnée dans le terminal
+    '''
     # Read
-    select_statement = database.select()
-    result_set = conn.execute(select_statement)
-    for r in result_set:
-        print(r)
+    messages = session.query(Message)  
+    for message in messages:  
+        print(message)
+
+# read_all_bdd()
+
+
+
+# https://www.compose.com/articles/using-postgresql-through-sqlalchemy/
+
+
+
 
